de90ec48614af3f30130278193eee1d75d7db342
AsylzhanUbaidullaev <72826853+AsylzhanUbaidullaev@users.noreply.github.com>
2020-12-09 22:54:43 +0600

    end

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -8,16 +8,49 @@ import {Movie} from '../../Movie';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  menus = ['/Adventure', '/Action', '/Horror', '/Comedy', '/Detective', '/Cartoon'];
+  menus = ['/Adventure', '/Action', '/Horror', '/Comedy', '/Detective', '/Cartoon', '/Dramas', '/Documentary', '/Family'];
   SearchMovie = '';
   name: string;
+
   constructor(private dataService: DataService) {
   }
 
   category: Movie [] = [];
 
-  ngOnInit(): void {
-    this.dataService.getMovie().subscribe((data: Movie) => this.category = data["Adventure"]);
+  check(index) {
+    switch (index) {
+      case 0:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Adventure']);
+        break;
+      case 1:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Action']);
+        break;
+      case 2:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Horror']);
+        break;
+      case 3:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Comedy']);
+        break;
+      case 4:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Detective']);
+        break;
+      case 5:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Cartoon']);
+        break;
+      case 6:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Dramas']);
+        break;
+      case 7:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Documentary']);
+        break;
+      case 8:
+        this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Family']);
+        break;
+      default:
+        break;
+    }
   }
-
-}
+    ngOnInit(): void {
+      this.dataService.getMovie().subscribe((data: Movie) => this.category = data['Adventure']);
+    }
+  }
